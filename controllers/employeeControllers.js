9152200fcd594df2c94e37e1449a25cc2123c34c
hashimshaji23 <hashimshaji12@gmail.com>
2026-06-25 03:02:58 +0530

    express work

--- controllers/employeeControllers.js
+++ controllers/employeeControllers.js
@@ -23,16 +23,89 @@ export const addEmployee = async (req, res, next) => {
     }
 }
 
-export const getEmployee = async (req, res, next)=>{
-    try{
+export const getEmployee = async (req, res, next) => {
+    try {
         const listEmployee = await Employee.find();
         res.status(200).json({
             status: true,
             message: "successfull",
             data: listEmployee
         })
-        
-    }catch(err){
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+export const singleEmployee = async (req, res, next) => {
+    try {
+        const { id } = req.body
+        if (!id) {
+            console.log("Id is required");
+
+        }
+        const employee = await Employee.findById(id)
+        if (!employee) {
+            console.log("Employee not found");
+        } else {
+            res.status(200).json({
+                status: true,
+                message: "success",
+                data: employee
+            })
+        }
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const updateEmployee = async (req, res, next) => {
+    try {
+        const { id, name, Rol, Gender } = req.body
+
+        if (!id) {
+            console.log("Id is required")
+        }
+        const updateData = {}
+        if (id) updateData.Id = id
+        if (name) updateData.name = name
+        if (Rol) updateData.Rol = Rol
+        if (Gender) updateData.Gender = Gender
+
+        const updatedData = await Employee.findByIdAndUpdate(id, updateData, {
+            new: true,
+        })
+        res.status(200).json({
+            status: true,
+            message: "success",
+            data: updateEmployee
+        })
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const deleteEmployee = async (req, res, next) => {
+    try {
+        const { id } = req.body
+        if (!id) {
+            console.log("Id is required")
+        }
+        const deleteemployee = await Employee.findByIdAndDelete(id)
+        if (!deleteEmployee) {
+            console.log("Employee not found");
+        } else {
+            res.status(200).json({
+                status: true,
+                message: "success",
+                data: deleteEmployee
+            })
+        }
+
+    } catch (err) {
         console.log(err)
     }
 }
--- routes/employeeRout.js
+++ routes/employeeRout.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { addEmployee, getEmployee } from "../controllers/employeeControllers.js";
+import { addEmployee, deleteEmployee, getEmployee, singleEmployee, updateEmployee } from "../controllers/employeeControllers.js";
 
 const router = Router()
 
 
 router.post('/add', addEmployee)
 router.get('/get', getEmployee)
+router.get('/single', singleEmployee)
+router.put('/edit', updateEmployee)
+router.delete('/delete', deleteEmployee)
 
 export default router
